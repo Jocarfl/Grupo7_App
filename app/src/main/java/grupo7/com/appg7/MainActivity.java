4ce19e431b3065a322c92d6539115fb3cdd43d62
Josep Carreres <jcarreresjr@hotmail.com>
2018-11-06 13:54:45 +0100

    Confirmación cerrar sesión funcionando

--- app/src/main/java/grupo7/com/appg7/MainActivity.java
+++ app/src/main/java/grupo7/com/appg7/MainActivity.java
@@ -1,11 +1,13 @@
 package grupo7.com.appg7;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -22,7 +24,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener  {
+        implements NavigationView.OnNavigationItemSelectedListener {
     private Button AcercaDe;
 
 
@@ -51,7 +53,7 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.contenedor, new Fragments_Inicio()).commit();
     }
 
@@ -92,7 +94,7 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    public void lanzarAcercaDe(View view){
+    public void lanzarAcercaDe(View view) {
         Intent i = new Intent(this, AcercaDeActivity.class);
         startActivity(i);
     }
@@ -103,14 +105,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         if (id == R.id.nav_home) {
 
             fragmentManager.beginTransaction().replace(R.id.contenedor, new Fragments_Inicio()).commit();
-        }
-
-        else if (id == R.id.nav_camera) {
+        } else if (id == R.id.nav_camera) {
 
             fragmentManager.beginTransaction().replace(R.id.contenedor, new Fragments01()).commit();
 
@@ -136,24 +136,47 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }else if (id == R.id.nav_cerrar_sesión) {
-            AuthUI.getInstance().signOut(MainActivity.this)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Intent i = new Intent(MainActivity.this,LoginActivity.class);
-                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                                    | Intent.FLAG_ACTIVITY_NEW_TASK
-                                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(i);
-                            MainActivity.this.finish();
-                        }
-                    });
+        } else if (id == R.id.nav_cerrar_sesión) {
+            AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+
+            dialogo1.setTitle("Advertencia");
+            dialogo1.setMessage("¿ Quiere cerrar sesión ?");
+            dialogo1.setCancelable(false);
+            dialogo1.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+                    aceptar();
+                }
+            });
+            dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogo1, int id) {
+
+                }
+            });
+            dialogo1.show();
+
 
         }
 
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+
         return true;
+    }
+
+    public void aceptar() {
+        AuthUI.getInstance().signOut(MainActivity.this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                                | Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(i);
+                        MainActivity.this.finish();
+                    }
+                });
     }
 }
